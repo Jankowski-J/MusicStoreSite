7e333ff81c27ed5a5b93d384ede0e594c888a33f
Chrisisar <krzysztofgornowicz@gmail.com>
2014-09-02 13:47:42 +0100

    Entity kick-off

---
+++ MusicStoreSite/Models/Contexts/MusicStoreContext.cs
@@ -0,0 +1,14 @@
+﻿using MusicStoreSite.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MusicStoreSite.Models.Contexts
+{
+    public class MusicStoreContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
--- MusicStoreSite/Models/Entities/Product.cs
+++ MusicStoreSite/Models/Entities/Product.cs
@@ -1,16 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace MusicStoreSite.Models.Entities
 {
-    class Product
+    public class Product
     {
         public int ProductId { set; get; }
+        [Required()]
         public string Artist { set; get; }
+        [Required()]
         public string Title { get; set; }
+        [Required()]
         public string Genre { get; set; }
+        [Required()]
         public decimal Price { get; set; }
+
+        public Product()
+        {
+        }
     }
 }
