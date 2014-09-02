97b8a7dc89a4b302a5214171ee3e9c040ec4fb86
robert-kozakiewicz <kozakiewicz.robert@gmail.com>
2014-09-02 13:14:30 +0200

    Added first entity

---
+++ MusicStoreSite/Models/Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MusicStoreSite.Models.Entities
+{
+    class Product
+    {
+        public int ProductId { set; get; }
+        public string Artist { set; get; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
