c9d4869da36c10816b73c302a3c50f55a0793ed7
Rudauke <jakubj93@gmail.com>
2014-09-02 13:46:59 +0100

    Added ProductDetails view

---
+++ MusicStoreSite/Controllers/StoreController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MusicStoreSite.Models.Entities;
+
+namespace MusicStoreSite.Controllers
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ProductDetails(int id = 0)
+        {
+            Product product = new Product() { Price = 10.0M, AddedAt = DateTime.Now, Artist = "Ozzy Osbourne", ProductId = 1001, Genre = "Rock", Title = "Paranoid" };
+
+            return View(product);
+        }
+    }
+}
