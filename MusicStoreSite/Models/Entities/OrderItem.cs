bcb78f92b9560ceb9f9c9b4e049231441ef616a6
Rudauke <jakubj93@gmail.com>
2014-09-04 11:01:24 +0100

    Added OrderItem

---
+++ MusicStoreSite/Models/Entities/OrderItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MusicStoreSite.Models.Entities
+{
+    public class OrderItem
+    {
+        [Key]
+        public int OrderItemId { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+
+        [Required]
+        public int OrderId { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+    }
+}
---
+++ MusicStoreSite/Scripts/Store/CheckoutScreen.js
@@ -0,0 +1,15 @@
+﻿var CheckoutScreen = (function () {
+
+    setupPage();
+
+    function setupPage() {
+        $("form").submit(function (e) {
+            if ($("#PromoCode").val() == '') {
+                e.preventDefault();
+                
+            } else if ($("#PromoCode").val() != 'FREE') {
+
+            }
+        });
+    };
+})
