af287b955bf016f783b059028d3f98acb4fc54d1
Chrisisar <krzysztofgornowicz@gmail.com>
2014-09-05 09:11:57 +0100

    db update

--- MusicStoreSite/Models/Contexts/MusicStoreContext.cs
+++ MusicStoreSite/Models/Contexts/MusicStoreContext.cs
@@ -15,11 +15,7 @@ namespace MusicStoreSite.Models.Contexts
         {
         }
 
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Order>().HasMany(o => o.OrderItemsList).WithRequired();
-            base.OnModelCreating(modelBuilder);
-        }
+        
         public DbSet<UserProfile> UserProfiles { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<Genre> Genres { get; set; }
